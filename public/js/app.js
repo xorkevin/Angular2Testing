5680172b9f59e3208cb72c666570b6e8827c701d
Kevin Wang <wangkevin448@gmail.com>
2015-09-12 00:36:01 -0700

    finally works

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'app'
+})
+@View({
+  template: '<h1>My First Angular2 App</h1>'
+})
+class AppComponent {
+
+}
+
+export {
+  AppComponent
+};
---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+import 'reflect-metadata';
+import {bootstrap} from 'angular2/angular2';
+import {AppComponent} from './app';
+
+console.log('hello');
+
+bootstrap(AppComponent);
